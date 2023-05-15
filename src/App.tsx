0789096960debe0cc8d8b499fb0e2e7e63b04f5f
Crpearce <Crpearce892@gmail.com>
2023-05-15 09:31:51 -0600

    render list card

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,17 @@ interface IState {
     name: string
     age: number
     url: string
-    notes? : string
+    note? : string
   }[]
 }
 
 function App() {
-  const [people, setPeople] = useState<IState["people"]>([])
+  const [people, setPeople] = useState<IState["people"]>([{
+    name: "Lebron James",
+    age: 36,
+    url: "https://www.gannett-cdn.com/presto/2023/05/13/USAT/63da4a57-f6e8-48ea-b70b-13c093876b94-AP_Warriors_Lakers_Basketball.jpg",
+    note: "Allergic to staying with the same team"
+  }])
 
   return (
     <div className="App">
